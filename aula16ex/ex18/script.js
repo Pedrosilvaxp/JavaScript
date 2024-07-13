5d9738c0dcce34a8bb42e8d25dd87067584ce258
Pedro Silva do Nascimento <phpedrojs@gmail.com>
2024-07-12 21:21:22 -0300

    up

--- aula16ex/ex18/script.js
+++ aula16ex/ex18/script.js
@@ -1,61 +1,29 @@
-function adicionar(chamada) {
-    let num = Number(document.getElementById('num').value);
-    let item = document.createElement('option');
+let num = document.querySelector('numero')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
 
-    if (num == 0) {
-        alert('[ERRO] Digite um valor por favor');
 
-    } else if (sele.innerHTML.includes(num) == false) {
-        item.text = `Valor ${num} adicionado`;
-        sele.appendChild(item);
-        numbers.push(num);
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(l, n) {
+    if (l.indexOf(n) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if(isNumero(num.value) && !inLista(num.value)) {
 
     } else {
-        alert(`[ERRO] Valor ${num} já adicionado`);
-    };
-};
-
-let sele = document.getElementById('sele');
-let numbers = [];
-
-function finalizar() {
-    // Total
-    var res1 = document.getElementById('res1');
-    let total = numbers.length;
-    if (total <= 1) {
-        res1.innerHTML = `Temos o total de apenas um valor`;
-    } else {
-        res1.innerHTML = `Temos o total de ${total} valores`;
-    };
-
-    // Maior 
-    var res2 = document.getElementById('res2');
-    let maior = numbers.sort();
-    res2.innerHTML = `O maior valor informado = ${maior[total-1]}`;
-
-    // Menor
-    var res3 = document.getElementById('res3');
-    res3.innerHTML = `O menor valor informado = ${maior[0]}`;
- 
-    // Soma de todos
-    var contagem = 0;
-    var n1 = 0;
-    var soma = 0;
-    while (contagem < total) {
-
-        if (total-contagem >= 2) {
-            soma += numbers[n1] + numbers[n1+1];
-        } else {
-            soma += numbers[n1];
-        };
-        n1 += 2;
-        contagem += 2;       
-    };
-    var res4 = document.getElementById('res4');
-    res4.innerHTML = `Todos os valores somados = ${soma}`;
-
-    // Média
-    var res5 = document.getElementById('res5');
-    let media = soma / total;
-    res5.innerHTML = `A média dos valores = ${media}`;
-};
+        alert('Valor inválido ou já encontrado na lista')
+    }
+}
